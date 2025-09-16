5e60d892f7308123ab29f0f67ffbb2d768657554
124Yasaswi <159883507+124Yasaswi@users.noreply.github.com>
2025-09-17 02:06:53 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,6 @@
+#leapyear
+year = 2024
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print(year, "is a Leap Year")
+else:
+    print(year, "is NOT a Leap Year")
---
+++ task2.py
@@ -0,0 +1,16 @@
+color = "black"
+
+if color == "Green":
+    print("you can go")
+elif color == "Yellow" :
+    print("get ready")
+elif color == "Red":
+    print("you must stop")
+else:
+    print("wrong color")
+    
+    
+    
+
+
+    
