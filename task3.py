439e2a78d4c3cece591aead2cf01fc2096960e62
124Yasaswi <159883507+124Yasaswi@users.noreply.github.com>
2025-09-18 11:03:13 +0530

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,14 @@
+n = 14
+
+if n>80 and n<=100:
+    print("You will get A grade")
+elif n>60 and n<=80:
+    print("you will get B grade")
+elif n>40 and n<=60:
+    print("you will get c grade")
+elif n>20 and n<=40:
+    print("you will get D grade")
+elif n>0 and n<20:
+    print("you will get E grade")
+else:
+    print("Invalid number")
---
+++ task5.py
@@ -0,0 +1,10 @@
+num = -70
+
+if num%7 == 0 and num%10 == 0:
+    print("Divisible by both 7 and 10")
+elif num%7 == 0:
+    print("only divisible by 7")
+elif num%10 ==0:
+    print("only divisible by 10")
+else:    
+     print("not divisible by both 7 and 10")
