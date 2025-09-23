7f35e60093e56a0e3e48bee09ba9e1c45c804423
124Yasaswi <159883507+124Yasaswi@users.noreply.github.com>
2025-09-23 11:26:47 +0530

    Add files via upload

---
+++ whilelooptask.py
@@ -0,0 +1,39 @@
+#-1 to -10 using while Loop
+
+a=-10
+while a<=-1:
+    print(a)
+    a=a+1
+    
+print()
+# -10 to -1 using while loop
+
+b=-1
+while b>=-10:
+    print(b)
+    b=b-1
+    
+# printing 1 t0 5 tables using while loop
+
+
+i=1
+while i<=5:
+    print("table of",i)
+    j=1
+    while j<=10:
+        print(i,"*",j,"=",i*j)
+        j=j+1
+    i=i+1
+    print()
+    
+#printing alphabets using while loop
+
+ch=ord('A')
+while ch<=ord("Z"):
+    print(chr(ch),end="")
+    ch=ch+1
+    print()
+    
+    
+    
+    
